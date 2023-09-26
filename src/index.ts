2d180f0162a334d53ed04f4bf7c867e1551e6ed1
hta218 <huynhtuananh21895@gmail.com>
2023-09-26 09:58:46 +0700

    Trying `commander`

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
 #!/usr/bin/env node
 
-const hello = () => {
-  console.log("Hello World!");
-};
+import { Command } from "commander";
 
-hello();
+// Define the program
+let program = new Command();
+
+function hello(message: string = "World") {
+  console.log(`Hello ${message}!`);
+}
+
+// Configure the program
+program
+  .name("My cli program")
+  .description("My first Nodejs x Typescript CLI program")
+  .version("1.0.0");
+
+// Add commands
+program
+  .argument("<message>", "The message to print")
+  .action((message: string) => {
+    hello(message);
+  });
+
+// Execute the program with the arguments
+program.parse(process.argv);
